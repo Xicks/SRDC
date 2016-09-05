673d00bbfd716eb9fc99042620e896852251b865
Bento Siqueira <bentor.siqueira@gmail.com>
2016-09-05 00:06:25 -0300

    Help Tela de Sensores Pressão, Diabete e Peso

---
+++ app/src/main/java/sdr/ufscar/dev/srdc/activity/HelpTelaSensores.java
@@ -0,0 +1,13 @@
+package sdr.ufscar.dev.srdc.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelpTelaSensores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_tela_sensores);
+    }
+}
